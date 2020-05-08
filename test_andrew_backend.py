2c7d3edc7af49e9ff3585e214bf891eb3351d804
dhambrick <44014535+dhambrick@users.noreply.github.com>
2020-05-08 03:15:23 -0500

    Update test_andrew_backend.py

--- test_andrew_backend.py
+++ test_andrew_backend.py
@@ -7,7 +7,7 @@ def test_fourier():
     data = np.asarray(list(range(1,6))).reshape([1,5])
     t1 = np.linspace(-np.pi,np.pi,100)
     
-    fourier1 = data[0] * (1.0/np.sqrt(2)) + data[1]*np.sin(t1) + data[2] * np.cos(t1) + data[3]*np.sin(2*t1) + data[4]*np.cos(2*t1)
+    fourier1 = data[0,0] * (1.0/np.sqrt(2)) + data[0,1]*np.sin(t1) + data[0,2] * np.cos(t1) + data[0,3]*np.sin(2*t1) + data[0,4]*np.cos(2*t1)
     t2,fourier2 = andrew_curve.genAndrewCurve("fourier", data)
     assert t1 == t2
     assert fourier1 == fourier2
