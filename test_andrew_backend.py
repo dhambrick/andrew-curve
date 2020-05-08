e6023da7fa87a91fa0bf83c4a5ad9dab5bfc9ea7
dhambrick <44014535+dhambrick@users.noreply.github.com>
2020-05-08 03:12:17 -0500

    Update test_andrew_backend.py

--- test_andrew_backend.py
+++ test_andrew_backend.py
@@ -4,7 +4,7 @@ from andrew_backend import AndrewCurve
 def test_fourier():
     andrew_curve = AndrewCurve()
     
-    data = np.asarray(list(range(1,6)))
+    data = np.asarray(list(range(1,6))).reshape([1,5])
     t1 = np.linspace(-np.pi,np.pi,100)
     
     fourier1 = data[0] * (1.0/np.sqrt(2)) + data[1]*np.sin(t1) + data[2] * np.cos(t1) + data[3]*np.sin(2*t1) + data[4]*np.cos(2*t1)
